469e352c47f2e6ba4cfae7dc8f045545ea581fdf
Tanoy Bosh <tanoybosh@gmail.com>
2021-07-03 22:49:32 +0530

    3rd
    3rd

--- server.js
+++ server.js
@@ -14,17 +14,31 @@ app.listen(port = 3000, function () {
 });
 
 app.get("/",function(req,res){
-    res.render(__dirname + "/views/home.ejs")
+    res.render(__dirname + "/views/firstpage.ejs")
 });
 
-app.get("/mainpage", function (req, res) {
-    res.render(__dirname + "/public/mainpage.ejs");
+app.get("/teacher",function(req,res){
+    res.render(__dirname + "/views/teacher/teacherhome.ejs")
+});
+app.get("/teacherlogin", function (req, res) {
+    res.render(__dirname + "/views/teacher/teacherlogin.ejs");
+});
+app.get("/teachersignup", function(req,res){
+    res.render(__dirname + "/views/teacher/teachersignup.ejs")
+});
+app.get("/teacherpage", function (req, res) {
+    res.render(__dirname + "/views/teacher/teacherpage.ejs");
 });
 
-app.get("/login", function (req, res) {
-    res.render(__dirname + "/views/login.ejs");
+app.get("/student",function(req,res){
+    res.render(__dirname + "/views/student/studenthome.ejs")
 });
-
-app.get("/signup", function(req,res){
-    res.render(__dirname + "/views/signup.ejs")
+app.get("/studentlogin", function (req, res) {
+    res.render(__dirname + "/views/student/studentlogin.ejs");
+});
+app.get("/studentsignup", function(req,res){
+    res.render(__dirname + "/views/student/studentsignup.ejs")
+});
+app.get("/studentpage", function (req, res) {
+    res.render(__dirname + "/views/student/studentpage.ejs");
 });
