9d28b86f45e7374c40fb40251eeeb1a4fd2bc689
Tanoy Bosh <tanoybosh@gmail.com>
2021-07-08 12:23:16 +0530

    upload page

--- server.js
+++ server.js
@@ -12,48 +12,48 @@ app.use(express.static("public"));
 app.use(upload());
 
 
-app.get("/",function(req,res){
+app.get("/", function (req, res) {
     res.render(__dirname + "/views/firstpage.ejs")
 });
 
-app.get("/teacher",function(req,res){
+app.get("/teacher", function (req, res) {
     res.render(__dirname + "/views/teacher/teacherhome.ejs")
 });
 app.get("/teacherlogin", function (req, res) {
     res.render(__dirname + "/views/teacher/teacherlogin.ejs");
 });
-app.get("/teachersignup", function(req,res){
+app.get("/teachersignup", function (req, res) {
     res.render(__dirname + "/views/teacher/teachersignup.ejs")
 });
 app.get("/teacherpage", function (req, res) {
     res.render(__dirname + "/views/teacher/teacherpage.ejs");
 });
 
-app.get("/student",function(req,res){
+app.get("/student", function (req, res) {
     res.render(__dirname + "/views/student/studenthome.ejs")
 });
 app.get("/studentlogin", function (req, res) {
     res.render(__dirname + "/views/student/studentlogin.ejs");
 });
-app.get("/studentsignup", function(req,res){
+app.get("/studentsignup", function (req, res) {
     res.render(__dirname + "/views/student/studentsignup.ejs")
 });
 app.get("/studentpage", function (req, res) {
     res.render(__dirname + "/views/student/studentpage.ejs");
 });
 
-app.post("/studentpage", function(req,res){
-    if (req.files){
+app.post("/studentpage", function (req, res) {
+    if (req.files) {
         console.log(req.files)
         var file = req.files.file
         var filename = file.name
         console.log(filename)
 
-        file.mv('./course/'+filename, function(err){
-            if (err){
+        file.mv('./course/' + filename, function (err) {
+            if (err) {
                 res.send(err)
-            }else{
-                res.send("file uploaded")
+            } else {
+                res.sendFile(__dirname + "/goback.html")
             }
         })
     }
